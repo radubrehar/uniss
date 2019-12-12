cf4cf89671cd69aeacc4d16fd4f9ac1963247a21
radubrehar <radu@jslog.com>
2019-12-13 00:21:10 +0200

    Start typescript implementation

---
+++ build.ts
@@ -0,0 +1,72 @@
+interface CSSPropertyRyle {
+  name: string;
+  values: string[];
+}
+
+const mediaQueries = [
+  {
+    name: "sm",
+    value: "@media (min-width: 640px)"
+  },
+  {
+    name: "md",
+    value: "@media (min-width: 768px)"
+  },
+  {
+    name: "lg",
+    value: "@media (min-width: 1024px)"
+  },
+  {
+    name: "xl",
+    value: " @media (min-width: 1280px)"
+  }
+];
+
+const pseudoStates = ["hover", "active", "visited", "focus", "focus-within"];
+
+const options = [
+  {
+    name: "display",
+    values: [
+      "block",
+      "inline-block",
+      "inline",
+      "flex",
+      "inline-flex",
+      "table",
+      "table-row",
+      "table-cell",
+      "none"
+    ]
+  }
+];
+
+const result = options.reduce(
+  (acc: string[], propertyRule: CSSPropertyRyle) => {
+    const simpleRules: string[] = [];
+    const mediaRules: string[] = [];
+    const pseudoStateRules: string[] = []; // todo
+    propertyRule.values.forEach(value => {
+      const text = `.${propertyRule.name}\\=${value} {
+  ${propertyRule.name}: ${value};
+}`;
+      simpleRules.push(text);
+      mediaQueries.forEach(mediaQuery => {
+        mediaRules.push(`${mediaQuery.value} {
+${text}          
+}`);
+      });
+    });
+
+    // @media (min-width: 1024px) {
+
+    acc.push(...simpleRules);
+
+    acc.push(...mediaRules);
+
+    return acc;
+  },
+  [] as string[]
+);
+
+require("fs").writeFileSync("./style.css", result.join("\n"));
